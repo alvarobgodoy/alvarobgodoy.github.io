62b86d4f4fa325dbcafe2beb8046523aee72edff
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2024-08-01 18:22:28 +0300

    setup: github pages & sass

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,16 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-secondary">Secondary</button>
+      <button type="button" class="btn btn-success">Success</button>
+      <button type="button" class="btn btn-danger">Danger</button>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <button type="button" class="btn btn-info">Info</button>
+      <button type="button" class="btn btn-light">Light</button>
+      <button type="button" class="btn btn-dark">Dark</button>
+
+      <button type="button" class="btn btn-link">Link</button>
     </>
   )
 }
