f9141222f035832906d69868f87f7869a70f3cb1
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2024-08-02 20:02:32 +0300

    add: about section inital content

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,8 +1,35 @@
+import Button from "../Button"
+import ConsoleImg from "/src/assets/console.png"
+
 export default function About() {
     return (
         <div className="section_about">
             <div className="container">
-                <h2>ABOUT ME! HAHA</h2>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <p className="subtitle">Get to know</p>
+                        <h2>About Me</h2>
+                        <p>My self-motivation and productive habits allow me to work remotely, always delivering quality work and doing it on time.  I had the opportunity to work for US-based clients for almost 1 year. I used technologies such as Sass, Bootstrap, Jquery, Drupal...</p>
+                        <Button link="#" type="primary">See portfolio</Button>
+                    </div>
+                    <div className="col-sm-6">
+                        <p>console goes here</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-4">
+                        <h4>Availability</h4>
+                        <p>My self-motivation and productive habits allow me to work remotely, always delivering quality work and doing it on time.</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <h4>Education</h4>
+                        <p>I'm a student at BYU-Idaho, majoring in Applied Technology. I've completed two certificates: Web & Computer Programming and Web Development.</p>
+                    </div>
+                    <div className="col-sm-4">
+                        <h4>Experience</h4>
+                        <p>I had the opportunity to work for US-based clients for almost 1 year. I used technologies such as Sass, Bootstrap, Jquery, Drupal...</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
