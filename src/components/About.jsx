f5d4c8914d84c1eb12ab8600ff7069730892b9d1
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2024-08-02 16:37:49 +0300

    setup: About section

---
+++ src/components/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div className="section_about">
+            <div className="container">
+                <h2>ABOUT ME! HAHA</h2>
+            </div>
+        </div>
+    )
+}
