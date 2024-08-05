10cb1fc3d4d2b9f2ceeef7d6626d5032d7f7ca66
Alvaro <76889519+alvarobgodoy@users.noreply.github.com>
2024-08-05 17:44:21 +0300

    styles: About & Testimonials

--- src/components/sections/Testimonials.jsx
+++ src/components/sections/Testimonials.jsx
@@ -1,23 +1,26 @@
 import Button from "../Button"
+import PersonIcon from "/src/assets/person-icon.svg"
 
 export default function Testimonials() {
     return (
         <div className="section_testimonials">
             <div className="container">
-                <div className="row">
+                <div className="row first-row">
                     <div className="col-12">
                         <p className="subtitle">Testimonials</p>
                         <h2>People I have worked with</h2>
                     </div>
                 </div>
-                <div className="row">
+                <div className="row second-row">
                     <div className="col-sm-6">
-                        <h4>Dario Godoy</h4>
+                        <img className="icon" src={PersonIcon} alt="Person Icon" />
+                        <h6>Dario Godoy</h6>
                         <p>"Always a student at BYU-Idaho, majoring in Applied Technology. I've completed two certificates."</p>
                         <Button link="#" type="link-dark">Lead Developer @ GCD</Button>
                     </div>
                     <div className="col-sm-6">
-                        <h4>Kelly Bell</h4>
+                        <img className="icon" src={PersonIcon} alt="Person Icon" />
+                        <h6>Kelly Bell</h6>
                         <p>"Always a student at BYU-Idaho, majoring in Applied Technology. I've completed two certificates."</p>
                         <Button link="#" type="link-dark">CTO @ GCD</Button>
                     </div>
